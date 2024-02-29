d6548c007c98a1a124f6ffd4a8b7c59ba73e56ba
Johan Vrolix <johan@vrolix.eu>
2024-02-29 16:11:26 +0100

    chore(backend-contacts-data-access) move lib to correct folder

--- libs/shared/backend/contacts/data-access/jest.config.ts
+++ libs/shared/backend/contacts/data-access/jest.config.ts
@@ -1,11 +1,12 @@
 /* eslint-disable */
 export default {
   displayName: 'backend-contacts-data-access',
-  preset: '../../../../jest.preset.js',
+  preset: '../../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../coverage/libs/backend/contacts/data-access',
+  coverageDirectory:
+    '../../../../../coverage/libs/backend/contacts/data-access',
 };
