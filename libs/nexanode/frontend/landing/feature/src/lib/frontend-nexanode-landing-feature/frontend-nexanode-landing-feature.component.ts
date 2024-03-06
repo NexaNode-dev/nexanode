105edf2052b8f7b75ad5eaa71dd5d2d7c945e8c6
Johan Vrolix <johan@vrolix.eu>
2024-03-06 14:28:12 +0100

    feat(nexanode-frontend): Init of app

--- libs/nexanode/frontend/landing/feature/src/lib/frontend-nexanode-landing-feature/frontend-nexanode-landing-feature.component.ts
+++ libs/nexanode/frontend/landing/feature/src/lib/frontend-nexanode-landing-feature/frontend-nexanode-landing-feature.component.ts
@@ -3,6 +3,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { bootstrapLinkedin, bootstrapTwitterX } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'nexanode-landing',
@@ -12,7 +14,9 @@ import { MatGridListModule } from '@angular/material/grid-list';
     MatButtonModule,
     MatCardModule,
     MatGridListModule,
+    NgIconComponent,
   ],
+  viewProviders: [provideIcons({bootstrapTwitterX, bootstrapLinkedin})],
   templateUrl: './frontend-nexanode-landing-feature.component.html',
   styleUrl: './frontend-nexanode-landing-feature.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
