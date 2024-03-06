b41e1317d8ef8e890d84881add4d0c3ee0f68acf
Johan Vrolix <johan@vrolix.eu>
2024-03-06 15:22:16 +0100

    fix(nexanode-frontend): Fix tests

--- apps/nexanode/frontend/src/app/app.component.spec.ts
+++ apps/nexanode/frontend/src/app/app.component.spec.ts
@@ -1,27 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent, RouterTestingModule],
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('should have router-outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome nexanode-frontend',
-    );
+    expect(compiled.querySelector('router-outlet')?.textContent).toBeDefined();
   });
 
-  it(`should have as title 'nexanode-frontend'`, () => {
+  it(`should have as title 'NexaNode'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nexanode-frontend');
+    expect(app.title).toEqual('NexaNode');
   });
 });
