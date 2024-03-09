d6aad2815cf0b839087857612e97a05dd90e20b7
Johan Vrolix <johan@vrolix.eu>
2024-03-09 22:10:22 +0100

    Format fixed

--- apps/optimalist/frontend/src/app/app.routes.ts
+++ apps/optimalist/frontend/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path:'',
-    loadChildren: () => import('@nexanode/optimalist-frontend-landing-feature').then(m => m.optimalistFrontendLandingFeatureRoutes)
+    path: '',
+    loadChildren: () =>
+      import('@nexanode/optimalist-frontend-landing-feature').then(
+        (m) => m.optimalistFrontendLandingFeatureRoutes,
+      ),
   },
   {
     path: 'about',
-    loadChildren: () => import('@nexanode/optimalist-frontend-about-feature').then(m => m.optimalistFrontendAboutFeatureRoutes)
-  }
+    loadChildren: () =>
+      import('@nexanode/optimalist-frontend-about-feature').then(
+        (m) => m.optimalistFrontendAboutFeatureRoutes,
+      ),
+  },
 ];
