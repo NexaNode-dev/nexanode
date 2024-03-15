712fd9b801912ac31583e1ca3e08c276d27b92d6
Johan Vrolix <johan@vrolix.eu>
2024-03-15 11:43:06 +0100

    Smooth scrolling

--- apps/optimalist/frontend/src/app/app.config.ts
+++ apps/optimalist/frontend/src/app/app.config.ts
@@ -1,11 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withInMemoryScrolling,
+} from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes, withComponentInputBinding()),
+    provideRouter(
+      appRoutes,
+      withComponentInputBinding(),
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      }),
+    ),
     provideHttpClient(),
   ],
 };
