ef00d07efa3baab0e4ed0368ab040e869b32d7bf
Johan Vrolix <johan@vrolix.eu>
2024-03-28 17:53:38 +0100

    style(stoic-frontend): styling using tailwind main preset

--- libs/stoic/frontend/landing/feature/tailwind.config.js
+++ libs/stoic/frontend/landing/feature/tailwind.config.js
@@ -1,26 +1,12 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
+const sharedTailwindConfig = require('../../../shared/frontend/tailwind/util/preset/tailwind.config');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [sharedTailwindConfig],
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    extend: {
-      height: {
-        'screen-70': '70vh',
-        '3/5': '60%',
-      },
-      minHeight: {
-        'screen-70': '70vh',
-      },
-      fontFamily: {
-        header: ['"Playfair Display"', 'serif'],
-        body: ['Merriweather', 'sans-serif'],
-      }
-    },
-  },
-  plugins: [],
 };
