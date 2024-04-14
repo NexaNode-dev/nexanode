8773dec2714298f450456548d775271f2e46314e
Johan Vrolix <johan@vrolix.eu>
2024-04-14 08:32:52 +0200

    feat(domain-interfaces): Added UserProfile interface

--- libs/shared/domain/interfaces/src/index.ts
+++ libs/shared/domain/interfaces/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/contact.interface';
 export * from './lib/user.interface';
 export * from './lib/role.interface';
 export * from './lib/permission.interface';
+export * from './lib/user-profile.interface';
