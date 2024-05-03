0de376fe3efee421ad84c6c93c6bd7bfa34b34ad
Johan Vrolix <johan@vrolix.eu>
2024-05-03 16:25:59 +0200

    test(optimalist-frontend): mocked ServicesStore

--- apps/optimalist/frontend/src/app/app.component.spec.ts
+++ apps/optimalist/frontend/src/app/app.component.spec.ts
@@ -1,11 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ServicesStore } from '@nexanode/optimalist-frontend-services-state';
 
 describe('AppComponent', () => {
+  const mockServicesStore = {
+    services: () => [],
+    getServices: () => [],
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, RouterTestingModule],
+      providers: [{ provide: ServicesStore, useValue: mockServicesStore }],
     }).compileComponents();
   });
 
