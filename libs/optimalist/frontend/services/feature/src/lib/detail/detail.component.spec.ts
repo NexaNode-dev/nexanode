af64cd20d415e5b7ede8a89adcd28543d3c2f1bb
Johan Vrolix <johan@vrolix.eu>
2024-05-03 16:41:33 +0200

    test(optimalist-frontend-services-feature): DetailComponent set input id

--- libs/optimalist/frontend/services/feature/src/lib/detail/detail.component.spec.ts
+++ libs/optimalist/frontend/services/feature/src/lib/detail/detail.component.spec.ts
@@ -1,17 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DetailComponent } from './detail.component';
+import { ServicesStore } from '@nexanode/optimalist-frontend-services-state';
+import { ViewportScroller } from '@angular/common';
+import { ComponentRef } from '@angular/core';
+
+window.scrollTo = jest.fn();
 
 describe('DetailComponent', () => {
   let component: DetailComponent;
+  let componentRef: ComponentRef<DetailComponent>;
   let fixture: ComponentFixture<DetailComponent>;
+
+  const mockServicesStore = {
+    selectedService: () => [],
+    otherServices: () => [],
+    getServiceById: () => [],
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DetailComponent],
+      providers: [{ provide: ServicesStore, useValue: mockServicesStore }, ViewportScroller],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DetailComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+    componentRef.setInput('id', '1');
     fixture.detectChanges();
   });
 
