1651f0e5d2559a0188b99b86401952bee289c06b
Johan Vrolix <johan@vrolix.eu>
2024-05-08 08:14:15 +0200

    feat(lifeperformance-frontend-landing-feature): Team component

--- libs/shared/lifeperformance/frontend/landing/feature/src/lib/landing.component.ts
+++ libs/shared/lifeperformance/frontend/landing/feature/src/lib/landing.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IntroComponent } from './intro/intro.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
+import { TeamComponent } from './team/team.component';
 
 @Component({
   selector: 'lifeperformance-landing',
   standalone: true,
-  imports: [IntroComponent, AboutComponent, ServicesComponent],
+  imports: [IntroComponent, AboutComponent, ServicesComponent, TeamComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
