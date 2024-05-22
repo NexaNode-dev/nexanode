2587c850faa6960ebb7dd56d2388ff527e0af566
Johan Vrolix <johan@vrolix.eu>
2024-05-22 22:22:41 +0200

    feat(lifeperformance-frontend-footer-feature): Added socials to footer

--- libs/lifeperformance/frontend/footer/feature/src/lib/footer.component.ts
+++ libs/lifeperformance/frontend/footer/feature/src/lib/footer.component.ts
@@ -1,12 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+  bootstrapInstagram,
+  bootstrapLinkedin,
+} from '@ng-icons/bootstrap-icons';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'lifeperformance-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgIconComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  viewProviders: [provideIcons({ bootstrapLinkedin, bootstrapInstagram })],
 })
 export class FooterComponent {}
