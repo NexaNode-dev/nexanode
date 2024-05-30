855061bd2bfc847baab35dc4801d072000fc9bd4
Johan Vrolix <johan@vrolix.eu>
2024-05-30 15:35:20 +0200

    test(tijdenlangverhaald-frontend-header-feature): fixed tests

--- libs/tijdenlangverhaald/frontend/header/feature/src/lib/header.component.spec.ts
+++ libs/tijdenlangverhaald/frontend/header/feature/src/lib/header.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
+import { provideRouter } from '@angular/router';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,6 +9,7 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HeaderComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
@@ -17,5 +19,13 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have 4 links', () => {
+    const links = fixture.nativeElement.querySelectorAll('a');
+    expect(links.length).toBe(4);
+  });
+  it('should have a title of "Tijdenlang Verhaald"', () => {
+    const title = fixture.nativeElement.querySelector('h1');
+    expect(title.textContent).toBe('Tijdenlang Verhaald');
   });
 });
