d17074fd032b30f6d7227c8fce6f2a8f624e6239
Johan Vrolix <johan@vrolix.eu>
2024-06-18 20:11:47 +0200

    init(backend-bookings-application): Initial setup of library

---
+++ libs/shared/backend/bookings/application/src/lib/backend-bookings-application.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { BookingsService } from './bookings.service';
+import { BackendBookingsDataAccessModule } from '@nexanode/backend-bookings-data-access';
+import { BackendPaymentsUtilModule } from '@nexanode/backend-payments-util';
+
+@Module({
+  imports: [
+    BackendBookingsDataAccessModule,
+    BackendPaymentsUtilModule.register({
+      paymentProvider: process.env['PAYMENT_PROVIDER'] || 'stripe',
+    }),
+  ],
+  providers: [BookingsService],
+  exports: [BookingsService],
+})
+export class BackendBookingsApplicationModule {}
