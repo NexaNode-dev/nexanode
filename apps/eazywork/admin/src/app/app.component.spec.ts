12f865cd1f76299e11ffb00cd361c2bf1c44d440
Johan Vrolix <johan@vrolix.eu>
2024-07-14 16:28:31 +0200

    test(easywork-admin): fix unit test

--- apps/eazywork/admin/src/app/app.component.spec.ts
+++ apps/eazywork/admin/src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, RouterTestingModule],
+      imports: [AppComponent],
+      providers: [provideRouter([]), provideHttpClient(), provideAnimations()],
     }).compileComponents();
   });
 
