a92e2de5b077d3a8c4a8ee0e7b8530273d9f7cc6
Johan Vrolix <johan@vrolix.eu>
2024-07-19 15:25:25 +0200

    chore(lifeperformance-backend): WebPack Config

--- apps/lifeperformance/backend/webpack.config.js
+++ apps/lifeperformance/backend/webpack.config.js
@@ -1,5 +1,6 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
+const { basename, join, resolve } = require('path');
+const { sync } = require('glob');
 
 module.exports = {
   output: {
@@ -14,6 +15,39 @@ module.exports = {
       assets: ['./src/assets'],
       optimization: false,
       outputHashing: 'none',
+      additionalEntryPoints: getEntryPoints(),
+      generatePackageJson: true,
     }),
   ],
 };
+
+function getEntryPoints() {
+  const entityFiles = sync(
+    resolve(
+      __dirname,
+      '../../../libs/shared/backend/{users,users-roles,roles,roles-permissions,permissions,media,services,events,bookings}/data-access/**/*.entity.ts'
+    )
+  );
+
+  const entities = entityFiles.reduce((acc, filename) => {
+    const entryName = `entities/${basename(filename, '.entity.ts')}`;
+    acc.push({
+      entryName: entryName,
+      entryPath: filename,
+    });
+    return acc;
+  }, []);
+
+  const migrationFiles = sync(resolve('./src/migrations/*.ts'));
+
+  const migrations = migrationFiles.reduce((acc, filename) => {
+    const migrationName = `migrations/${basename(filename, '.entity.ts')}`;
+    acc.push({
+      entryName: migrationName,
+      entryPath: filename,
+    });
+    return acc;
+  }, []);
+
+  return [...entities, ...migrations];
+}
