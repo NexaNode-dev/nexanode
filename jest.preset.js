b1065a6bbbbc9883c1eb3bb6e8787f4c6360a94c
Johan Vrolix <johan@vrolix.eu>
2024-07-24 16:53:09 +0200

    chore(@nexanode/source): modeleNameMapper try 3 jest.preset.js

--- jest.preset.js
+++ jest.preset.js
@@ -3,13 +3,13 @@ const nxPreset = require('@nx/jest/preset').default;
 module.exports = { ...nxPreset,
   moduleNameMapper: {
     '^lit$':
-      '<projectRoot>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+      'libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
     '^lit-html$':
-      '<projectRoot>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+      'libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
     '^lit-element$':
-      '<projectRoot>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+      'libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
     '^lit/decorators.js$':
-      '<projectRoot>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+      'libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
     '^lit-html/directives/unsafe-html.js$':
-      '<projectRoot>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+      '/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
   }, };
