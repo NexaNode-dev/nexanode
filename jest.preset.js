d8902410f5eeabfe3a2d3d14140bd9995e5ccb93
Johan Vrolix <johan@vrolix.eu>
2024-07-24 16:42:45 +0200

    chore(@nexanode/source): rootDir to projectRoot in jest.preset.js

--- jest.preset.js
+++ jest.preset.js
@@ -3,13 +3,13 @@ const nxPreset = require('@nx/jest/preset').default;
 module.exports = { ...nxPreset,
   moduleNameMapper: {
     '^lit$':
-      '<rootDir>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+      '<projectRoot>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
     '^lit-html$':
-      '<rootDir>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+      '<projectRoot>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
     '^lit-element$':
-      '<rootDir>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+      '<projectRoot>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
     '^lit/decorators.js$':
-      '<rootDir>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+      '<projectRoot>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
     '^lit-html/directives/unsafe-html.js$':
-      '<rootDir>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+      '<projectRoot>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
   }, };
