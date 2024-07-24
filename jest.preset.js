0bb4ef360fa8ed5ee97ec775e1b711bfa85012fc
Johan Vrolix <johan@vrolix.eu>
2024-07-24 16:32:58 +0200

    Global mocking of lit for Angular feature libs

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,15 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = { ...nxPreset,
+  moduleNameMapper: {
+    '^lit$':
+      '<rootDir>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+    '^lit-html$':
+      '<rootDir>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+    '^lit-element$':
+      '<rootDir>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+    '^lit/decorators.js$':
+      '<rootDir>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+    '^lit-html/directives/unsafe-html.js$':
+      '<rootDir>/libs/shared/testing/lit-mocks/utils/src/lib/lit-element-mock.ts',
+  }, };
