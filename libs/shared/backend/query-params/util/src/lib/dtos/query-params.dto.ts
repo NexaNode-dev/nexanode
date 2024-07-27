9fdf368fffa8555132e578b375d56bd029421eea
Johan Vrolix <johan@vrolix.eu>
2024-07-27 14:14:28 +0200

    feat(backend-query-params-util): Optimise DTO

--- libs/shared/backend/query-params/util/src/lib/dtos/query-params.dto.ts
+++ libs/shared/backend/query-params/util/src/lib/dtos/query-params.dto.ts
@@ -4,8 +4,120 @@ import {
   RelationKeys,
   WhereCondition,
   OrderCondition,
+  OrderDirection,
 } from '@nexanode/domain-interfaces';
-import { IsOptional, IsString, IsNumber, IsBoolean } from 'class-validator';
+import {
+  IsOptional,
+  IsString,
+  IsNumber,
+  IsBoolean,
+  ValidateNested,
+  IsArray,
+  IsObject,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+class ConditionOperatorsDto {
+  @IsOptional()
+  @IsString()
+  gt?: string;
+
+  @IsOptional()
+  @IsString()
+  lt?: string;
+
+  @IsOptional()
+  @IsString()
+  like?: string;
+}
+
+class WhereConditionDto<T> {
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => ConditionOperatorsDto)import {
+    IQueryParams,
+    ScalarKeys,
+    RelationKeys,
+    WhereCondition,
+    OrderCondition,
+  } from '@nexanode/domain-interfaces';
+  import {
+    IsOptional,
+    IsString,
+    IsNumber,
+    IsBoolean,
+    ValidateNested,
+    IsArray,
+  } from 'class-validator';
+  import { Type } from 'class-transformer';
+  
+  class ConditionOperatorsDto {
+    @IsOptional()
+    @IsString()
+    gt?: string;
+  
+    @IsOptional()
+    @IsString()
+    lt?: string;
+  
+    @IsOptional()
+    @IsString()
+    like?: string;
+  }
+  
+  class WhereConditionDto<T> {
+    @IsOptional()
+    @ValidateNested({ each: true })
+    @Type(() => ConditionOperatorsDto)
+    [key: string]: any | ConditionOperatorsDto;
+  }
+  
+  class OrderConditionDto<T> {
+    @IsOptional()
+    @IsString()
+    [key: string]: 'ASC' | 'DESC' | OrderConditionDto<T>;
+  }
+  
+  export class QueryParamsDto<T> implements IQueryParams<T> {
+    @IsOptional()
+    @IsString({ each: true })
+    select?: ScalarKeys<T>[];
+  
+    @IsOptional()
+    @ValidateNested({ each: true })
+    @Type(() => WhereConditionDto)
+    where?: WhereConditionDto<T>;
+  
+    @IsOptional()
+    @IsString({ each: true })
+    relations?: RelationKeys<T>[];
+  
+    @IsOptional()
+    @ValidateNested({ each: true })
+    @Type(() => OrderConditionDto)
+    order?: OrderConditionDto<T>;
+  
+    @IsOptional()
+    @IsBoolean()
+    withDeleted?: boolean;
+  
+    @IsOptional()
+    @IsNumber()
+    skip?: number;
+  
+    @IsOptional()
+    @IsNumber()
+    take?: number;
+  }
+  
+  [key: string]: any | ConditionOperatorsDto;
+}
+
+class OrderConditionDto<T> {
+  @IsOptional()
+  @IsString()
+  [key: string]: OrderDirection | OrderConditionDto<T>;
+}
 
 export class QueryParamsDto<T> implements IQueryParams<T> {
   @IsOptional()
@@ -13,13 +125,17 @@ export class QueryParamsDto<T> implements IQueryParams<T> {
   select?: ScalarKeys<T>[];
 
   @IsOptional()
-  where?: WhereCondition<T>;
+  @ValidateNested({ each: true })
+  @Type(() => WhereConditionDto)
+  where?: WhereCondition<T> | WhereCondition<T>[];
 
   @IsOptional()
   @IsString({ each: true })
   relations?: RelationKeys<T>[];
 
   @IsOptional()
+  @ValidateNested()
+  @Type(() => OrderConditionDto)
   order?: OrderCondition<T>;
 
   @IsOptional()
