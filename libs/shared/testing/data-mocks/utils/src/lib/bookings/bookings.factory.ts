d2ef7e2179d52f5dc5df5bf76d148d88daf133bd
Johan Vrolix <johan@vrolix.eu>
2024-07-30 18:55:24 +0200

    feat(testing-data-mocks-utils): aligned Booking Factory to new Booking Interface

--- libs/shared/testing/data-mocks/utils/src/lib/bookings/bookings.factory.ts
+++ libs/shared/testing/data-mocks/utils/src/lib/bookings/bookings.factory.ts
@@ -13,6 +13,9 @@ export const bookingFactory = (options?: Partial<IBooking>): IBooking => {
     ]) as 'pending' | 'confirmed' | 'cancelled',
     createdAt: faker.date.recent(),
     updatedAt: faker.date.recent(),
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    units: faker.number.int({ min: 1, max: 10 }),
   };
 
   return { ...booking, ...options };
