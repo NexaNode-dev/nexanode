dc19361351e18969fab61aa44208142b08120e7a
Johan Vrolix <johan@vrolix.eu>
2024-09-24 17:08:29 +0200

    feat(eazywork-frontend-landing-feature): Animations

--- libs/eazywork/frontend/landing/feature/src/landing/benefits/benefits.component.ts
+++ libs/eazywork/frontend/landing/feature/src/landing/benefits/benefits.component.ts
@@ -1,5 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'eazywork-benefits',
@@ -7,6 +14,78 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './benefits.component.html',
   styleUrl: './benefits.component.css',
+  animations: [
+    trigger('slideInAnimation', [
+      state(
+        'out',
+        style({
+          opacity: 0,
+          transform: 'translateY(20%)',
+        }),
+      ),
+      state(
+        'in',
+        style({
+          opacity: 1,
+          transform: 'translateY(0)',
+        }),
+      ),
+      transition('out => in', animate('600ms ease-out')),
+    ]),
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EazyworkBenefitsComponent {}
+export class EazyworkBenefitsComponent implements OnInit {
+  benefits = [
+    {
+      title: 'Voor Bedrijven',
+      description: `
+      Ontdek en huur snel en kosteneffectief gekwalificeerde professionals in.
+      Ons platform zorgt ervoor dat je kandidaten ontmoet die worden beoordeeld
+      op hun vaardigheden, niet op hun achtergrond.
+    `,
+    },
+    {
+      title: 'Voor Werkzoekenden',
+      description: `
+      Jouw Vaardigheden, Jouw Kansen. Toon je expertise en maak verbinding met
+      toonaangevende bedrijven in diverse sectoren. Bij Eazywork richten we ons
+      op jouw vaardigheden en ervaring, niet op je achtergrond, om je te helpen
+      de juiste baan te vinden die past bij je carrière-ambities. Of je nu op
+      zoek bent naar een remote functie of een nieuwe uitdaging, wij bieden een
+      platform waar jouw capaciteiten centraal staan.
+    `,
+    },
+    {
+      title: 'Voor Freelancers',
+      description: `
+      Toon je vaardigheden en vind remote opdrachten die aansluiten bij jouw
+      expertise. We geloven in een op verdiensten gebaseerd systeem waarin jouw
+      kennis en ervaring de sleutel zijn tot het ontgrendelen van kansen.
+    `,
+    },
+  ];
+
+  inView: boolean[] = [false, false, false];
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkInView();
+  }
+
+  ngOnInit() {
+    this.checkInView();
+  }
+
+  checkInView() {
+    const elements = this.el.nativeElement.querySelectorAll('.benefits div');
+    elements.forEach((element: HTMLElement, index: number) => {
+      const rect = element.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 100 && rect.bottom >= 0) {
+        this.inView[index] = true;
+      }
+    });
+  }
+}
--- libs/eazywork/frontend/landing/feature/src/landing/hero/hero.component.ts
+++ libs/eazywork/frontend/landing/feature/src/landing/hero/hero.component.ts
@@ -1,3 +1,4 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
@@ -6,6 +7,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
+  animations: [
+    trigger('fadeInAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('2000ms ease-out', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EazyWorkHeroComponent {}
